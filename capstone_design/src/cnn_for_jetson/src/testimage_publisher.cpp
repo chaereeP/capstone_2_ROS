b7266c227bc98bc253e9db99879954744e6d95a4
leeJaeDuck <roy9109@kaist.ac.kr>
2018-08-14 23:40:32 +0900

    v2

---
+++ capstone_design/src/cnn_for_jetson/src/testimage_publisher.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
+#include <string>
+#include <signal.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <ros/ros.h>
+#include <boost/thread.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "ros/ros.h"
+
+
+#include "sensor_msgs/CompressedImage.h"
+
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+
+    ros::init(argc, argv, "test");
+    ros::NodeHandle n;
+    ros::Publisher pub;
+    pub = n.advertise<sensor_msgs::CompressedImage>("catdog/image", 1); //setting publisher
+
+    cv::Mat image;
+    image = cv::imread("/home/naverlabs/ROS_code/capstone_design/src/cnn_for_jetson/src/cat.325.jpg",CV_LOAD_IMAGE_COLOR);
+
+    while(ros::ok){
+
+      sensor_msgs::CompressedImage msg;
+
+      cv::imencode(".jpg", image, msg.data);
+      pub.publish(msg);
+      cv::imshow("Frame",image);
+      cv::waitKey(30);
+
+      ros::spinOnce();
+    }
+
+    return 0;
+}
